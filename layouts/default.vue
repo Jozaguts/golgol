97dab0b159e6d8e5f2d102c7a43a4e3fc0482483
Sagit Gutierrez <jozaguts@gmail.com>
2022-10-29 22:45:03 -0500

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-navigation-drawer location="right" app> nav </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-title><h1 class="title">GOL GOL</h1></v-app-bar-title>
+    </v-app-bar>
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <slot />
+      </v-container>
+    </v-main>
+    <v-footer app></v-footer>
+  </v-app>
+</template>
+
+<script lang="ts" setup></script>
